693c0363ed8d44378745f4cd92f9d5d2b174d98c
anshul051 <anshulkumararya51@gmail.com>
2025-12-12 16:50:13 +0530

    server and data model setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import morgan from 'morgan';
+
+dotenv.config();
+
+//connect to MongoDB
+connectDB();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+//test route
+app.get('/test', (req,res) => {
+    res.send('API is running...');
+});
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server is running on : " + PORT);
+});
